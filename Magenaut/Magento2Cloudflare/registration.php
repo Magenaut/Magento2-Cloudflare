797cd0b37177ae81543c5a76c8ee8f5457723b07
Jared Chu <me@jaredchu.com>
2020-09-19 16:52:26 +0700

    Initial

---
+++ Magenaut/Magento2Cloudflare/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magenaut Team All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magenaut_Magento2Cloudflare', __DIR__);
+
